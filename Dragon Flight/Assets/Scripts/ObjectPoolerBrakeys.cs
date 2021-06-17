3130e9e0d10f88a040375d285a135edcd8fd44e0
AntiProgresant <alexanderlane614@gmail.com>
2021-06-16 22:26:34 -0400

    file rename

--- Dragon Flight/Assets/Scripts/ObjectPoolerBrakeys.cs
+++ Dragon Flight/Assets/Scripts/ObjectPoolerBrakeys.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPoolerBrakeys : MonoBehaviour
+public class ObjectPooler : MonoBehaviour
 {
     [System.Serializable]
     public class Pool
